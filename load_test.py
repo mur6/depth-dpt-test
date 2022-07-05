773f55fce78c8d6ffcebedf35a35face6181dc8a
MURAKI Taichi <muraki.taichi@gmail.com>
2022-07-05 16:18:14 +0900

    f

--- load_test.py
+++ load_test.py
@@ -35,44 +35,15 @@ print("Train Size   : ", len(X_train))
 print("Val Size     : ", len(X_val))
 print("Test Size    : ", len(X_test))
 
-from torch.utils.data import Dataset, DataLoader
-import cv2
-
-
-class DroneDataset(Dataset):
-    def __init__(self, root_dir, X):
-        self.img_path = root_dir / "train_images"
-        self.depth_path = root_dir / "train_depth"
-        self.mask_path = root_dir / "train_masks"
-        self.X = X
-
-    def __len__(self):
-        return len(self.X)
-
-    def __getitem__(self, idx):
-        id = self.X[idx]
-        img = cv2.imread(self.img_path / f"image_{id:06}.jpg")
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        mask = cv2.imread(self.mask_path / f"image_{id:06}.png", cv2.IMREAD_GRAYSCALE)
-
-        if self.transform is not None:
-            aug = self.transform(image=img, mask=mask)
-            img = Image.fromarray(aug["image"])
-            mask = aug["mask"]
-
-        if self.transform is None:
-            img = Image.fromarray(img)
-
-        # t = T.Compose([T.ToTensor(), T.Normalize(self.mean, self.std)])
-        t = T.ToTensor()
-        img = t(img)
-        mask = torch.from_numpy(mask).long()
-        return img, mask
+# from torch.utils.data import Dataset, DataLoader
+# import cv2
 
 
 # datasets
-train_set = DroneDataset(root_dir, X_train)
-val_set = DroneDataset(root_dir, X_val)
+from finetune.datasets import SimpleDepthDataset
+
+train_set = SimpleDepthDataset(root_dir, X_train)
+val_set = SimpleDepthDataset(root_dir, X_val)
 
 # train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 # val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=True)
